c00dbbde7e1687dffca03725e451acc952e55f40
Alisa "Awesome" Palson and Roy van de Water <sqrt+alisa+roy@octoblu.com>
2015-04-06 13:44:46 -0700

    Support the repo name RPM structure

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
-aws s3 sync s3://$S3_BUCKET_NAME /tmp/rpms
-createrepo /tmp/rpms
-aws s3 sync /tmp/rpms s3://$S3_BUCKET_NAME
+BUCKET_URL=s3://${S3_BUCKET_NAME}
+
+aws s3 sync $BUCKET_URL /tmp/rpms
+
+mkdir -p /tmp/rpms/repodata
+mv /tmp/rpms/repodata /tmp/rpms/${S3_LOCAL_PATH}/
+
+createrepo /tmp/rpms/${S3_LOCAL_PATH}
+
+mv /tmp/rpms/${S3_LOCAL_PATH}/repodata /tmp/rpms/
+
+aws s3 sync /tmp/rpms $BUCKET_URL
