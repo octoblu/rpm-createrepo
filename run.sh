cc37db79791b847185587e921b311329127a38bb
Alisa "Awesome" Palson and Roy van de Water <sqrt+alisa+roy@octoblu.com>
2015-04-06 13:46:42 -0700

    Never mind

--- run.sh
+++ run.sh
@@ -3,12 +3,5 @@
 BUCKET_URL=s3://${S3_BUCKET_NAME}
 
 aws s3 sync $BUCKET_URL /tmp/rpms
-
-mkdir -p /tmp/rpms/repodata
-mv /tmp/rpms/repodata /tmp/rpms/${S3_LOCAL_PATH}/
-
-createrepo /tmp/rpms/${S3_LOCAL_PATH}
-
-mv /tmp/rpms/${S3_LOCAL_PATH}/repodata /tmp/rpms/
-
+createrepo /tmp/rpms
 aws s3 sync /tmp/rpms $BUCKET_URL
